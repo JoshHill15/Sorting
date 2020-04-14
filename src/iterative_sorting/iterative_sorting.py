b5cf616e618ddf0c466e6be5a62246e17345562b
Josh <josh_hill15@me.com>
2020-04-14 19:03:56 -0400

    mvp

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,5 +1,7 @@
 # TO-DO: Complete the selection_sort() function below
-# arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+arr1 = [1, 1, 5, 2, 4]
+arr2 = ['Bob', 'Slack', ['reddit', '89', 101, [
+    'alacritty', '(brackets)', 5, 375]], 0, ['{slice, owned}'], 22]
 #  - 10
 
 
@@ -20,23 +22,66 @@ def selection_sort(arr):
     return arr
 
 
-# print(selection_sort(arr1))
+# print("selection", selection_sort(arr1))
 # TO-DO:  implement the Bubble Sort function below
 
 
 def bubble_sort(arr):
     length = len(arr)
-    for i in range(0, length):
+    for i in range(length):
         for j in range(1, length - i):
             if (arr[j-1] > arr[j]):
                 arr[j], arr[j-1] = arr[j-1], arr[j]
     return arr
 
 
-# print(bubble_sort(arr1))
+# print("bubble", bubble_sort(arr1))
 # STRETCH: implement the Count Sort function below
 
+# arr1 = [1, 1, 5, 2, 4]
 
-def count_sort(arr, maximum=-1):
+def count_sort(array):
 
-    return arr
+    size = len(array)
+    output = [0] * size
+    count = [0] * 10
+
+    for i in range(0, size):
+        count[array[i]] += 1
+
+    for i in range(1, 10):
+        count[i] += count[i - 1]
+
+    i = size - 1
+    while i >= 0:
+        output[count[array[i]] - 1] = array[i]
+        count[array[i]] -= 1
+        i -= 1
+
+    for i in range(0, size):
+        array[i] = output[i]
+    return output
+
+
+print("count", count_sort(arr1))
+
+
+# def nested_print(arr):
+#     for element in arr:
+#         if type(element) == list:
+#             nested_print(element)
+#         else:
+#             print(element)
+
+# def nested_print(arr):
+#     for element in arr:
+#         # if element == list:
+#         #     print('list')
+#         while type(element) == list and element.length > 1:
+#             for i in element:
+#                 print(i)
+#         else:
+#             print(element)
+
+
+# print("nest", nested_print(arr2))
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,26 +1,71 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
+arr1 = [1, 2, 8, 5, 78]
+arr2 = [3, 5, 9, 34]
+
+
+def merge(arrA, arrB):
+    merged_arr = [None] * (len(arrA) + len(arrB))
     # TO-DO
-    
+    # Initalize pointers
+    first, second, third = 0, 0, 0
+    # while first is smaller than first array and second is smaller than second array
+    while first < len(arrA) and second < len(arrB):
+        # if element in first array is smaller than element in second array
+        if arrA[first] < arrB[second]:
+            merged_arr[third] = arrA[first]
+            first += 1
+            third += 1
+        # if element in second array is smaller than element in first array
+        else:
+            merged_arr[third] = arrB[second]
+            third += 1
+            second += 1
+    # if second array is larger than first, add the remaining values in second array to merged_arr
+    if first >= len(arrA):
+        while second < len(arrB):
+            merged_arr[third] = arrB[second]
+            third += 1
+            second += 1
+    # if first array is larger than second, add the remaining values in first array to merged_arr
+    elif second >= len(arrB):
+        while first < len(arrA):
+            merged_arr[third] = arrA[first]
+            third += 1
+            first += 1
     return merged_arr
 
 
+# print("merge", merge(arr1, arr2))
+
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
+
+
+# arr1 = [1, 2, 8, 5,78]
+def merge_sort(arr):
     # TO-DO
+    # if length of array is 1 or 0, return it
+    if len(arr) <= 1:
+        return arr
+    # keep slicing array in the middle, until one element per array done via recursion
+    middle = len(arr) // 2
+    rhs = arr[middle:]
+    lhs = arr[:middle]
+    # build sorted array out of arrays with length of one with merge function
+    return merge(merge_sort(lhs), merge_sort(rhs))
 
-    return arr
 
-
+# merge(lhs, rhs)
+print("merge_sort", merge_sort(arr1))
 # STRETCH: implement an in-place merge sort algorithm
+
+
 def merge_in_place(arr, start, mid, end):
     # TO-DO
 
     return arr
 
-def merge_sort_in_place(arr, l, r): 
+
+def merge_sort_in_place(arr, l, r):
     # TO-DO
 
     return arr
@@ -28,6 +73,25 @@ def merge_sort_in_place(arr, l, r):
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort( arr ):
+def timsort(arr):
 
     return arr
+
+
+def quicksort(arr):
+    # Base case: arr len 0 or 1 is sorted
+    if len(arr) <= 1:
+        return arr
+    # Pick a pivot
+    pivot = arr[0]
+    # Separate all vals smaller and larger than pivot
+    smaller = []
+    larger = []
+    for i in range(1, len(arr)):
+        if arr[i] <= pivot:
+            smaller.append(arr[i])
+        else:
+            larger.append(arr[i])
+    # Sort smaller and larger
+    # Concatenate smaller + pivot + larger
+    return quicksort(smaller) + [pivot] + quicksort(larger)
